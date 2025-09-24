d4dd3c552a781fddb33d0b1aaac482a0166d60b4
Maddox Bartoli <mtbartoli@cps.edu>
2025-09-24 10:56:14 -0500

    a2.py 2

--- a2.py
+++ a2.py
@@ -25,26 +25,51 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
         # your job is to fill out the body of this loop
 
         # you should delete the following line
-        return ["Not done yet :)"]
+
 
         # 1) if we reached the end of the pattern but not source
-
+    if pind == len(pattern) and sind < len(source):
+        return None
         # 2) if the current thing in the pattern is a %
         # WARNING: this condition contains the bulk of the code for the assignment
         # If you get stuck on this one, we encourage you to attempt the other conditions
         #   and come back to this one afterwards
 
+    elif pattern[pind] == "%":
+        if pind == len(pattern) - 1: # if % is the last element in pattern
+                combined = " ".join(source[sind:])
+                result.append(combined)
+                return result
+        else:
+            pind += 1
+            accum = ""
+            while pattern[pind] != source[sind]:
+                    accum += source[sind] + " "
+                    sind += 1
+
+                    if sind == len(source):
+                        return None
+                
+                    result.append(accum.strip())
+
         # 3) if we reached the end of the source but not the pattern
-
+    elif sind == len(source):
+        return None
         # 4) if the current thing in the pattern is an _
-
+    elif pattern[pind] == "_":
+            result.append(source[sind])
+            pind += 1
+            sind += 1
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source
-
+    elif pattern[pind] == source[sind]:
+            pind += 1
+            sind += 1
         # 6) else : this will happen if none of the other conditions are met it
         # indicates the current thing it pattern doesn't match the current thing in
         # source
-
+    else:
+        return None
     return result
 
 
